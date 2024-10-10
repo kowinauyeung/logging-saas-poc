f921dc302545debb1caf87bd1989633c26c13d26
kowinauyeung <auyeung@tomody.com>
2024-10-10 17:02:45 +0900

    feat: add testing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const fireErrorIfEven = useCallback(() => {
+    const nextCount = count + 1
+    setCount(nextCount)
+    if (count % 2 !== 0) {
+      const msg = `This is even number ${nextCount}!!`
+      console.log('log: ' + msg)
+      console.info('info: ' + msg)
+      console.warn('warn: ' + msg)
+      console.error('error: ' + msg)
+      console.trace('trace: ' + msg)
+      throw new Error(msg)
+    }
+  }, [count])
 
   return (
     <>
@@ -18,9 +31,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={fireErrorIfEven}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
