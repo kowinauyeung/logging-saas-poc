a20f0f20c6bc4fc8255fdc38f75fee35420723e6
kowinauyeung <auyeung@tomody.com>
2024-10-11 15:30:42 +0900

    feat: add sentry log data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { loggly } from './loggly'
 import './App.css'
+import * as Sentry from '@sentry/react'
+import { faker } from '@faker-js/faker'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +30,42 @@ function App() {
     }
   }, [count])
 
+  const logRandomly = useCallback(() => {
+    Sentry.captureMessage(`HOME_RANDOM_CLICK_${Date.now()}`, {
+      user: {
+        id: faker.database.mongodbObjectId(),
+        email: faker.internet.email(),
+        username: faker.internet.userName(),
+      },
+      tags: {
+        action: faker.hacker.verb(),
+        session_id: faker.string.uuid(),
+      },
+      extra: {
+        live_duration: faker.number.int({ min: 999999999 }),
+        live_times: faker.number.int({ min: 1, max: 100 }),
+      },
+    })
+  }, [])
+
+  const log = useCallback(() => {
+    Sentry.captureMessage(`HOME_BUTTON_CLICK_${Date.now()}`, {
+      user: {
+        id: 'thisisauserid',
+        email: 'john.doe@example.com',
+        username: 'John Doe',
+      },
+      tags: {
+        action: 'home_btn_click',
+        session_id: 'thisisasessionid',
+      },
+      extra: {
+        live_duration: 899999998,
+        live_times: 4,
+      },
+    })
+  }, [])
+
   return (
     <>
       <div>
@@ -40,7 +78,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={fireErrorIfEven}>count is {count}</button>
+        <button onClick={log}>Log</button>
+        <button onClick={logRandomly}>Random</button>
+        <button onClick={fireErrorIfEven}>Log error</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
